aeb07cbe354a422fb89a5d5c5dc7f40841a98f46
Ravi Sawlani <ravisawlani04@gmail.com>
2021-05-25 18:32:20 +0530

    update bs-local runner

--- scripts/local.runner.js
+++ scripts/local.runner.js
@@ -6,11 +6,11 @@ var bs_local;
 
 try {
 
-  process.mainModule.filename = "./node_modules/nightwatch/bin/nightwatch"
+  require.main.filename = "./node_modules/nightwatch/bin/nightwatch"
   // Code to start browserstack local before start of test
   console.log("Connecting local");
   Nightwatch.bs_local = bs_local = new browserstack.Local();
-  bs_local.start({'key': process.env.BROWSERSTACK_ACCESS_KEY }, function(error) {
+  bs_local.start({'key': process.env.BROWSERSTACK_KEY }, function(error) {
     if (error) throw error;
 
     console.log('Connected. Now testing...');
