af8c1e48beb24f7b49a772762c7a46d9f3782663
tayal1996 <tayalm08@gmail.com>
2021-07-15 13:00:07 +0530

    format: chained script browser commands

--- tests/suite/test_03.js
+++ tests/suite/test_03.js
@@ -1,14 +1,15 @@
 module.exports = {
   "Google's Search Functionality": function (browser) {
-    browser.url("https://www.google.com");
-    browser.setValue("input[name=q]", ["BrowserStack 03", browser.Keys.ENTER]); // this submits on desktop browsers
-    browser.pause(1000);
-    browser.title((result) => {
-      if (!/BrowserStack 03/i.test(result.value)) {
-        browser.submitForm("input[name=q]"); // this helps in mobile browsers
-      }
-    });
-    browser.expect.title().to.match(/BrowserStack 03/i);
+    browser
+      .url("https://www.google.com")
+      .setValue("input[name=q]", ["BrowserStack 03", browser.Keys.ENTER]) // this submits on desktop browsers
+      .pause(1000)
+      .title((result) => {
+        if (!/BrowserStack 03/i.test(result.value)) {
+          browser.submitForm("input[name=q]"); // this helps in mobile browsers
+        }
+      })
+      .expect.title().to.match(/BrowserStack 03/i);
     browser.end();
   },
 };
